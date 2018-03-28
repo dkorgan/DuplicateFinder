1cf76d361635b06a9bae53d9a92351cdcccc12f5
dk.obrien <32497532+dkorgan@users.noreply.github.com>
2018-03-28 10:06:27 -0700

    Add project files.

---
+++ Duplicate Finder/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioColumnOne = new System.Windows.Forms.RadioButton();
+            this.radioColumn2 = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Find Duplicates in:";
+            // 
+            // radioColumnOne
+            // 
+            this.radioColumnOne.AutoSize = true;
+            this.radioColumnOne.Location = new System.Drawing.Point(156, 17);
+            this.radioColumnOne.Name = "radioColumnOne";
+            this.radioColumnOne.Size = new System.Drawing.Size(83, 17);
+            this.radioColumnOne.TabIndex = 2;
+            this.radioColumnOne.TabStop = true;
+            this.radioColumnOne.Text = "Column One";
+            this.radioColumnOne.UseVisualStyleBackColor = true;
+            // 
+            // radioColumn2
+            // 
+            this.radioColumn2.AutoSize = true;
+            this.radioColumn2.Location = new System.Drawing.Point(156, 42);
+            this.radioColumn2.Name = "radioColumn2";
+            this.radioColumn2.Size = new System.Drawing.Size(84, 17);
+            this.radioColumn2.TabIndex = 3;
+            this.radioColumn2.TabStop = true;
+            this.radioColumn2.Text = "Column Two";
+            this.radioColumn2.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 83);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(15, 166);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(211, 282);
+            this.textBox1.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.radioColumn2);
+            this.Controls.Add(this.radioColumnOne);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioColumnOne;
+        private System.Windows.Forms.RadioButton radioColumn2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
